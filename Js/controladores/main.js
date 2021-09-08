3ae62f5aebdcf123221ffe5cabb61a7dc6e82448
Cgalvispadilla <fktcg99@gmail.com>
2021-09-08 15:20:39 -0500

    Se establecio distintos modos de dificultad y se agregaron nuevas instrucciones

--- Js/controladores/main.js
+++ Js/controladores/main.js
@@ -3,9 +3,12 @@ import { BoardView } from "../vistas/BoardView.js";
 import { Bar } from "../modelos/Bar.js";
 import { Ball } from "../modelos/Ball.js";
 
+var i;
+//const d_facil = document.dificultad.i_dificultad[1].checked;
+
 let board = new Board(800, 400);
-let bar = new Bar(780, 100, 20, 100, board);
-let bar2 = new Bar(0, 100, 20, 100, board);
+var bar = new Bar(780, 100, 20, 100, board);
+var bar2 = new Bar(0, 100, 20, 100, board);
 //console.log(board);
 let canvas = document.getElementById("canvas");
 let board_view = new BoardView(canvas, board);
@@ -19,7 +22,7 @@ document.addEventListener("keydown", (ev) => {
     }
   } else if (ev.keyCode === 40) {
     ev.preventDefault();
-    if (300 !== bar.y) {
+    if (400 - bar.height !== bar.y) {
       bar.down();
     }
   } else if (ev.keyCode === 87) {
@@ -29,19 +32,28 @@ document.addEventListener("keydown", (ev) => {
     }
   } else if (ev.keyCode === 83) {
     ev.preventDefault();
-    if (300 !== bar2.y) {
+    if (400 - bar.height !== bar2.y) {
       bar2.down();
     }
   } else if (ev.keyCode === 32) {
     ev.preventDefault();
     board.playing = !board.playing;
+    if (document.dificultad.i_dificultad[2].checked) {
+      bar.height = 40;
+      bar2.height = 40;
+    } else if (document.dificultad.i_dificultad[1].checked) {
+      bar.height = 70;
+      bar2.height = 70;
+    } else if (document.dificultad.i_dificultad[0].checked) {
+      bar.height = 100;
+      bar2.height = 100;
+    }
   }
-  console.log(bar2.toString());
 });
 
 window.requestAnimationFrame(controller);
 function controller() {
   board_view.play();
   window.requestAnimationFrame(controller);
-  console.log(ball.x + "y " + ball.y);
+  //console.log(ball.x + "y " + ball.y);
 }
